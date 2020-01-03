90523410b4666a25974340647830303974fb74a8
teerus <z83@foxmail.com>
2020-01-03 18:28:27 +0800

    初始化项目

---
+++ src/test/java/com/mygroup/zxy/knowledge/KnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygroup.zxy.knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
